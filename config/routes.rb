d4662644c63f7386caddc5917ce06896a491a91a
andreaainara <andrea.ainara10@gmail.com>
2016-05-24 15:25:08 -0700

    added api scope to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   root 'site#angular'
 
+  # api/ urls and default to JSON
+  scope '/api', defaults: { format: :json } do
+    resources :lists, except: [:new, :edit] do
+      resources :items, except: [:new, :edit]
+    end
+  end
+
   get '*path', to: 'site#angular'
 end
