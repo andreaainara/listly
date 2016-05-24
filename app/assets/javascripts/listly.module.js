653ff6416fb61d0226e01226a7246bd3af4f0947
andreaainara <andrea.ainara10@gmail.com>
2016-05-24 15:05:54 -0700

    configuring routes in listly.module.js

--- app/assets/javascripts/listly.module.js
+++ app/assets/javascripts/listly.module.js
@@ -1,1 +1,22 @@
-angular.module('ListlyApp', ['ngRoute']);
+angular.module('ListlyApp', ['ngRoute', 'templates'])
+  .config(config);
+
+  config.$inject = ['$routeProvider', '$locationProvider'];
+  function config (  $routeProvider,   $locationProvider  )  {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'lists.template.html',
+        controller: 'ListsController',
+        controllerAs: 'listsCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+
+
+    $locationProvider
+      .html5Mode({
+        enabled: true,
+        requireBase: false
+      });
+  }
